0555a2f157f153b935f111a5087f39c6513534d8
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-09-24 23:19:05 +0530

    Added: Single User Starting Position is Initialized to Zero

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,7 +3,10 @@ package AssignmentTwo;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-
+		
+		// variables
+		// Initialized User Starting Position
+		int user_position = 0;
 	}
 
 }
